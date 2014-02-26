147516017722698be3642b636dfd684222060b30
mpg2899 <mpg2899@email.vccs.edu>
2014-02-25 20:33:47 -0500

    Week 7 notes.

--- csc202/src/assignment1/TestProgram.java
+++ csc202/src/assignment1/TestProgram.java
@@ -6,16 +6,86 @@
  * 
  * This class is the driver to test our Fraction class and our NumericalOperations interface
  */
+
 package assignment1;
+
+import java.util.*;
 
 public class TestProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Read input
+		Scanner keyboard;
+		boolean validnums =false;
+		int num1 = 0, num2 = 0, den1 = 0, den2 = 0;
+		while (validnums == false) {
+			num1 = 0;
+			keyboard = new Scanner(System.in);
+			System.out.println("Enter first numerator");
+			try {
+				num1 = keyboard.nextInt();
+				validnums = true;
+			}
+			catch (Exception e) {
+				System.out.println("Invalid number entered");
+				num1 = 0;
+			}
+			
+		}
+		
+		validnums = false;
+		while (validnums == false) {
+			den1 = 0;
+			keyboard = new Scanner(System.in);
+			System.out.println("Enter first denominator");
+			try {
+				den1 = keyboard.nextInt();
+				validnums = true;
+			}
+			catch (Exception e) {
+				System.out.println("Invalid number entered");
+				den1 = 0;
+			}
+			
+		}
+		
+		validnums = false;
+		while (validnums == false) {
+			num2 = 0;
+			keyboard = new Scanner(System.in);
+			System.out.println("Enter second numerator");
+			try {
+				num2 = keyboard.nextInt();
+				validnums = true;
+			}
+			catch (Exception e) {
+				System.out.println("Invalid number entered");
+				num2 = 0;
+			}
+			
+		}
+		
+		validnums = false;
+		while (validnums == false) {
+			den2 = 0;
+			keyboard = new Scanner(System.in);
+			System.out.println("Enter second denominator");
+			try {
+				den2 = keyboard.nextInt();
+				validnums = true;
+			}
+			catch (Exception e) {
+				System.out.println("Invalid number entered");
+				den2 = 0;
+			}
+			
+		}
+		
 		//Create two fractions
-		NumericalOperations mynumber1 = new Fraction(3, 4);
-		NumericalOperations mynumber2 = new Fraction(3, 8);
+		NumericalOperations mynumber1 = new Fraction(num1, den1);
+		NumericalOperations mynumber2 = new Fraction(num2, den2);
 		
 		// Add the fractions
 		Fraction result = (Fraction) mynumber1.add(mynumber2);
--- csc202/src/week4/test1.java
+++ csc202/src/week4/test1.java
@@ -1,9 +1,20 @@
 package week4;
+import java.util.*;
 
 public class test1 extends testParent implements testinterface  {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		try {
+			int x = keyboard.nextInt();
+		}
+		
+		catch (Exception e) {
+			
+			System.out.println("Bad input");
+		}
+
 		float a = 2;
 		for (int i = 1; i < 127; i++) {
 			a = a * 2;
